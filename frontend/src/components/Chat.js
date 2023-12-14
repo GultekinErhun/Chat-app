8de506d59426571463c5d3a2bad322690ad80a7b
Barış Kılıç <baariskilicc@gmail.com>
2023-12-14 23:19:16 +0300

    delete if statement

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -246,7 +246,7 @@ function Chat() {
 
   const sendMessage = (selectedUser, enterMessage) => {
     // Check if the WebSocket is open
-    if (ws.current && ws.current.readyState === WebSocket.OPEN && selectedUser!='') {
+    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
       
       const formattedDate = currentDate.toLocaleString();
 
